ebca93aa7761273de1004e90e0e4c4a53571c705
sahgon <giorgos.korn@yahoo.gr>
2021-01-29 19:25:01 +0200

    C_Multithreading

---
+++ C_Multithreading/p3170074-p3170151.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define seat 10
+#define tel 8
+
+#define nSeatLow 1
+#define nSeatHigh 5
+
+#define tSeatLow 5
+#define tSeatHigh 10
+
+#define pCardSucces 0.9
+
+#define cash 4
+
+#define tCashLow 2
+#define tCashHigh 4
+
+#define zoneA 5
+#define zoneB 10
+#define zoneC 10
+
+#define cZoneA 30
+#define cZoneB 25
+#define cZoneC 20
+
+#define pZoneA 0.2
+#define pZoneB 0.4
+#define pZoneC 0.4
